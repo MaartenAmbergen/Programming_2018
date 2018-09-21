41dce3e2affd9054523a82585b467eac58f39017
Maarten <Maarten.ambergen@student.hu.nl>
2018-09-21 10:38:59 +0200

    blablabla

--- pe6_2.py
+++ pe6_2.py
@@ -1,1 +1,7 @@
 def som(getallenLijst):
+    antwoord=0
+    for i in getallenLijst:
+        antwoord= antwoord+i
+    print(antwoord)
+
+som([1,2,3,4,5,6,7,8,9,200,9999999999999999999999999999999999999999999999999999])
