ab77a79012110bbf85657d1f8ee946fdb35f746f
Maarten <Maarten.ambergen@student.hu.nl>
2018-09-24 09:03:41 +0200

    blablabla

--- pe6_6.py
+++ pe6_6.py
@@ -1,1 +1,12 @@
 def wijzig(letterlijst):
+    list.clear(letterlijst)
+    lijst2 = ['d', 'e', 'f']
+    for letter in lijst2:
+        letterlijst.append(letter)
+
+
+
+lijst=['a', 'b', 'c']
+print(lijst)
+wijzig(lijst)
+print(lijst)
