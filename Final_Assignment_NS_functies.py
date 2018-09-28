33f811a8e9cb6d723d1e4322c68c90d9d07b826a
Maarten <Maarten.ambergen@student.hu.nl>
2018-09-28 09:14:59 +0200

    FA1

---
+++ Final_Assignment_NS_functies.py
@@ -0,0 +1,35 @@
+leeftijd_input = float(input("Hoe oud ben je? "))
+dag_van_de_week_input = input("Welke dag is het vandaag? ")
+afstandKM_input = float(input("Hoe veel kilometer gaan we vandaag reizen? "))
+if dag_van_de_week_input in ["zaterdag", "Zaterdag", "zondag", "Zondag"]:
+    weekendrit_input = True
+else:
+    weekendrit_input = False
+
+def standaardtarief(afstandKM):
+    'Bepaalt het standaardbedrag voor een bepaalde rit'
+    if afstandKM < 0:
+        kosten = 0
+    elif afstandKM >50:
+        kosten = 15.0+(0.6*afstandKM)
+    else:
+        kosten = 0.8*afstandKM
+    return kosten
+
+def ritprijs(leeftijd, weekendrit, afstandKM):
+    standaardprijs = standaardtarief(afstandKM)
+    if leeftijd <12 or leeftijd >= 65:
+        if weekendrit == True:
+            te_betalen_prijs = 0.65*standaardprijs
+        else:
+            te_betalen_prijs = 0.70*standaardprijs
+    else:
+        if weekendrit == True:
+            te_betalen_prijs = 0.60*standaardprijs
+        else:
+            te_betalen_prijs = standaardprijs
+    return te_betalen_prijs
+
+print("je gaat vandaag €" +str(round(ritprijs(leeftijd_input, weekendrit_input, afstandKM_input), 2))+ " betalen!")
+
+
